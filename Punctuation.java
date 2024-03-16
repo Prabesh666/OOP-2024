db28ce2684982733ef92125e3c99a6eb19877486
Prabesh Shah <115727555+Prabesh666@users.noreply.github.com>
2024-03-16 20:55:53 +0545

    mistake

--- Punctuation.java
+++ Punctuation.java
@@ -1,27 +1,18 @@
-public class Punctuation {
+public class Diamond {
+    public static void main(String args[]) {
+        int a = 4;
 
-    public static void main(String[] args) {
-        String text = "Mary had a little lamb, her fleece was as white as snow, and everywhere Mary went, the lamb was sure to go. -that was a nice poem- the end. ";
+        for (int i = 0; i < a; i++) {
 
-        String punctuations = "!,.?-()";
+            for (int j = 0; j < a - i - 1; j++)
 
-        int[] counting_punct = new int[punctuations.length()];
-        for (int i = 0; i < counting_punct.length; i++) {
-            counting_punct[i] = 0;
-        }
-
-        for (char ch : text.toCharArray()) {
-            int index = punctuations.indexOf(ch);
-            if (index != -1) {
-                counting_punct[index]++;
+            {
+                System.out.println(" ");
             }
-        }
-
-        System.out.println("Punctuation | Count");
-        System.out.println("------------------");
-
-        for (int i = 0; i < punctuations.length(); i++) {
-            System.out.println(punctuations.charAt(i) + " | " + counting_punct[i]);
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
